838fe24724008b682b02515f2be5a19d865f93af
KazeHubuki <georgeyuanzj@icloud.com>
2025-03-22 21:33:27 -0700

    Checkpoint finished

--- proj1/deque/Deque.java
+++ proj1/deque/Deque.java
@@ -5,7 +5,11 @@ import java.util.Iterator;
 public interface Deque<T> extends Iterable<T>{
     int size();
 
+    @Override
+    Iterator<T> iterator();
 
+    @Override
+    boolean equals(Object o);
 
     default boolean isEmpty() {
         return (size() == 0);
